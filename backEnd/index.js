24aee7cdc4be7d8ca371727329f0e3e66d3bc464
Beula Samuel <092834@bun2-206-01.acg.ac.nz>
2021-08-12 11:03:46 +1200

    mongo connected

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,10 +1,11 @@
 const express =require('express');//includes express
 const app = express(); //calls the express method
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 //cross origin resource sharing
 const cors = require('cors');//cross origin restriction to be waived
 const bcrypt = require('bcryptjs');
-
+const config = require('./config.json');
 const product = require('./Products.json')
 
 const port = 3000;
@@ -21,6 +22,17 @@ app.use(bodyParser.urlencoded({extended:false}));//using default
 app.use(cors()); //calling cors method
 
 app.get('/',(req,res)=> res.send('Hello! I am from the backend'))
+
+
+ mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net/School?retryWrites=true&w=majority`, {useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=>console.log('DB connected!'))
+.catch(err=>{
+  console.log(`DBConnectionError:${err.message}`);
+});
+
+
+
+
 
 //get method to access data from Products.json
 //routing to the endpoint
